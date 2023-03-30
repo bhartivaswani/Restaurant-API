c6c5a930dbff1d7126ecce8884b23c18d9d23e2f
Deep Panchani <dlplearning00@gmail.com>
2023-03-30 19:27:40 +0530

    Created seprate routes

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ connectDB()
 
 const app = express()
 
-
+app.user('/api/user', require('./routes/userRoutes'))
+app.user('/api/order', require('./routes/orderRoutes'))
+app.user('/api/menu', require('./routes/menuRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
