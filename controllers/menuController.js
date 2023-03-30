f07e73f4a892a7f03e0a4beac0a5879168d35c48
Deep Panchani <dlplearning00@gmail.com>
2023-03-31 00:58:10 +0530

    Menu API 90%

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -10,12 +10,11 @@ const Menu = require('../models/menuModel')
 // @access Public
 const getMenu = asyncHandler( async (req,res) => {
     const menus = await Menu.find()
-    console.log(menus)
     res.status(200).json(menus)
 })
 
 // @desc Update Item
-// @route PUT /api/menu
+// @route PUT /api/menu/id
 // @access Public
 const updateItem = asyncHandler( async (req,res)=>{
     const menuItem = await Menu.findById(req.params.id)
@@ -30,7 +29,7 @@ const updateItem = asyncHandler( async (req,res)=>{
 })
 
 // @desc Delete Menu
-// @route DELETE /api/menu
+// @route DELETE /api/menu/id
 // @access Public
 const deleteItem = asyncHandler( async (req,res)=>{
     const menuItem = await Menu.findById(req.params.id)
@@ -40,7 +39,7 @@ const deleteItem = asyncHandler( async (req,res)=>{
         throw new Error('Item not found')
     }
 
-    await menuItem.remove()
+    const deleteItem = await Menu.findByIdAndRemove(req.params.id)
 
     res.status(200).json({id : req.params.id})
 })
@@ -49,6 +48,11 @@ const deleteItem = asyncHandler( async (req,res)=>{
 // @route POST /api/menu
 // @access Public
 const addItem = asyncHandler( async (req,res)=>{
+    if(req.body.name===undefined || req.body.type===undefined || req.body.price===undefined || req.body.available===undefined || req.body.servingSize===undefined){
+        res.status(400)
+        throw new Error('Incomplete request')
+    }
+
     const menu = await Menu.create({
         name: req.body.name,
         type: req.body.type,
